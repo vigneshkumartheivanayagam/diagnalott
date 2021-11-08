a69a195299fcdd1865c3eca8141b68ae6c87bd23
vigneshenfin <vignesh@enfintechnologies.com>
2021-11-08 23:41:32 +0530

    commit

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -94,7 +94,7 @@ function Home(props) {
         //     getContent()
         // }
 
-        if (e.target.offsetHeight + e.target.scrollTop >= e.target.clientHeight) {  
+        if (e.target.clientHeight + e.target.scrollTop === e.target.scrollHeight) {  
             getContent()
         }
 
