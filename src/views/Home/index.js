76011720ccf8443522d9857be329decdb2156c1a
vigneshenfin <vignesh@enfintechnologies.com>
2021-11-08 20:14:43 +0530

    commit

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -44,8 +44,6 @@ function Home(props) {
     }
 
     const handleScroll = (e) => {
-
-        alert('innerheight ==> ' + window.innerHeight + 'scrollY ==> ' + window.scrollY + 'offsetHeight ==> ' + document.body.offsetHeight)
         if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
             alert("Condition satisfied")
             getContent()
