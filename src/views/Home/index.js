7f569feea023d5a509be0b26c4baffa7059c2e27
vigneshenfin <vignesh@enfintechnologies.com>
2021-11-08 20:33:14 +0530

    commit

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -10,6 +10,7 @@ function Home(props) {
     const dispatch = useDispatch()
     let hasMore = React.useRef(true)
     let page = React.useRef(1)
+    const [loading, setLoading] = React.useState(false)
 
     React.useEffect(() => {
         window.addEventListener('scroll', handleScroll)
@@ -27,24 +28,29 @@ function Home(props) {
 
     const getContent = async () => {
 
-        try {
-            if (hasMore.current) {
-                let url = 'data/CONTENTLISTINGPAGE-PAGE' + page.current+'.json'
-                const result = await axios.get(url);
-                if (result !== undefined && result !== null && result.status !== undefined && result.status !== null && result.status === 200 && result.data !== undefined && result.data !== null && result.data.page !== undefined && result.data.page !== null && result.data.page['content-items'] !== undefined && result.data.page['content-items'] !== null && result.data.page['content-items'].content !== undefined && result.data.page['content-items'].content !== null && result.data.page['content-items'].content.length > 0) {
-                    page.current = parseInt(page.current) + 1
-                    dispatch(addContents(result.data.page['content-items'].content))
-                } else {
-                    hasMore.current = false
+        if(!loading) {
+            setLoading(true)
+            try {
+                if (hasMore.current) {
+                    let url = 'data/CONTENTLISTINGPAGE-PAGE' + page.current+'.json'
+                    const result = await axios.get(url);
+                    if (result !== undefined && result !== null && result.status !== undefined && result.status !== null && result.status === 200 && result.data !== undefined && result.data !== null && result.data.page !== undefined && result.data.page !== null && result.data.page['content-items'] !== undefined && result.data.page['content-items'] !== null && result.data.page['content-items'].content !== undefined && result.data.page['content-items'].content !== null && result.data.page['content-items'].content.length > 0) {
+                        page.current = parseInt(page.current) + 1
+                        dispatch(addContents(result.data.page['content-items'].content))
+                    } else {
+                        hasMore.current = false
+                    }
+                    setLoading(false)
                 }
+            } catch(e) {
+                hasMore.current = false
+                setLoading(false)
             }
-        } catch(e) {
-            hasMore.current = false
         }
     }
 
     const handleScroll = (e) => {
-        if (window.innerHeight + window.scrollY >= (document.body.offsetHeight - 50)) {
+        if (window.innerHeight + window.scrollY >= (document.body.offsetHeight - 10)) {
             alert("Condition satisfied")
             getContent()
         }
